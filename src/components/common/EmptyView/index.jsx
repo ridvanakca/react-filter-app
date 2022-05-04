c10604572fb383c103a6873876203047bb7e275a
Rıdvan Akca <akca.ridvann@gmail.com>
2022-05-04 17:21:14 +0300

    filters are appiled

--- src/components/common/EmptyView/index.jsx
+++ src/components/common/EmptyView/index.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import "./styles.css";
 
 const EmptyView = () => {
-  return <div>EmptyView</div>;
+  return (
+    <div className='emptyView-wrap'>
+      <img src='/images/gif/empty.gif' alt='empty view' />
+    </div>
+  );
 };
 
 export default EmptyView;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,15 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import FilterPanel from "../../components/Home/FilterPanel";
 import SearchBar from "../../components/Home/SearchBar";
 import List from "../../components/Home/List";
 import "./styles.css";
 import { dataList } from "../../data";
+import EmptyView from "../../components/common/EmptyView";
 
 const Home = () => {
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [selectedRating, setSelectedRating] = useState(null);
   const [selectedPrice, setSelectedPrice] = useState([1000, 5000]);
   const [list, setList] = useState(dataList);
+  const [inputSearch, setInputSearch] = useState("");
+  const [resultFound, setResultFound] = useState(false);
   const [cuisines, setCuisines] = useState([
     {
       id: 1,
@@ -24,7 +27,7 @@ const Home = () => {
     {
       id: 3,
       checked: false,
-      label: "Italien",
+      label: "Italian",
     },
   ]);
 
@@ -47,17 +50,55 @@ const Home = () => {
     setSelectedPrice(value);
   }
 
+  function applyFilter() {
+    let updatedList = dataList;
+
+    // rating filter
+    if (selectedRating) {
+      updatedList = updatedList.filter((item) => parseInt(item.rating) === parseInt(selectedRating));
+    }
+
+    // category filter
+    if (selectedCategory) {
+      updatedList = updatedList.filter((item) => item.category === selectedCategory);
+    }
+
+    // cuisine filter
+    // ['american', 'chinese']
+    const checkedCuisine = cuisines.filter((item) => item.checked).map((item) => item.label.toLowerCase());
+    if (checkedCuisine.length) {
+      updatedList = updatedList.filter((item) => checkedCuisine.includes(item.cuisine));
+    }
+
+    // price filter
+    const minPrice = selectedPrice[0];
+    const maxPrice = selectedPrice[1];
+
+    updatedList = updatedList.filter((item) => item.price >= minPrice && item.price <= maxPrice);
+
+    // inputSearch filter
+    if (inputSearch) {
+      updatedList = updatedList.filter((item) => item.title.toLowerCase().search(inputSearch.toLowerCase().trim()) !== -1);
+    }
+
+    setList(updatedList);
+
+    !updatedList.length ? setResultFound(false) : setResultFound(true);
+  }
+
+  useEffect(() => {
+    applyFilter();
+  }, [selectedRating, selectedCategory, cuisines, selectedPrice, inputSearch]);
+
   return (
     <div className='home'>
-      <SearchBar />
+      <SearchBar value={inputSearch} changeInput={(e) => setInputSearch(e.target.value)} />
 
       <div className='home_panelList-wrap'>
         <div className='home_panel-wrap'>
           <FilterPanel selectToggle={handleSelectCategory} selectedCategory={selectedCategory} selectRating={handleSelectRating} selectedRating={selectedRating} cuisines={cuisines} changeChecked={handleChangeChecked} selectedPrice={selectedPrice} changedPrice={handleChangedPrice} />
         </div>
-        <div className='home_list-wrap'>
-          <List list={list} />
-        </div>
+        <div className='home_list-wrap'>{resultFound ? <List list={list} /> : <EmptyView />}</div>
       </div>
     </div>
   );
